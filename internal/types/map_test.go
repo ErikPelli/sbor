0622b1f228581f7d241920fef8dc69eded9293f2
ErikPelli <erikpellizzon1@gmail.com>
2022-02-24 20:40:34 +0100

    Change map test, according to new implementation

--- internal/types/map_test.go
+++ internal/types/map_test.go
@@ -8,69 +8,69 @@ import (
 
 func TestMap_WriteTo_Simple(t *testing.T) {
 	data := []writeTestData{
-		{Map(map[MessagePackType]MessagePackType{}),
+		{Map([]MessagePackMap{}),
 			[]byte{0x80},
 		},
-		{Map(map[MessagePackType]MessagePackType{
-			String("int"): Uint(1),
+		{Map([]MessagePackMap{
+			{String("int"), Uint(1)},
 		}),
 			[]byte{0x81, 0xA3, 0x69, 0x6E, 0x74, 0x01},
 		},
-		{Map(map[MessagePackType]MessagePackType{
-			String("boolean"): Boolean(true),
-			String("null"):    Nil{},
-			String("string"):  String("foo bar"),
-			String("float"):   Float{F: 0.5},
+		{Map([]MessagePackMap{
+			{String("boolean"), Boolean(true)},
+			{String("null"), Nil{}},
+			{String("string"), String("foo bar")},
+			{String("float"), Float{F: 0.5}},
 		}),
 			[]byte{0x84, 0xA7, 0x62, 0x6F, 0x6F, 0x6C, 0x65, 0x61, 0x6E, 0xC3, 0xA4, 0x6E, 0x75, 0x6C, 0x6C, 0xC0, 0xA6, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0xA7, 0x66, 0x6F, 0x6F, 0x20, 0x62, 0x61, 0x72, 0xA5, 0x66, 0x6C, 0x6F, 0x61, 0x74, 0xCB, 0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 		},
-		{Map(map[MessagePackType]MessagePackType{
-			String("boolean"): Boolean(true),
-			String("null"):    Nil{},
-			String("string"):  String("foo bar"),
-			String("int"):     Int(-1),
-			String("float"):   Float{F: 0.5},
+		{Map([]MessagePackMap{
+			{String("boolean"), Boolean(true)},
+			{String("null"), Nil{}},
+			{String("string"), String("foo bar")},
+			{String("int"), Int(-1)},
+			{String("float"), Float{F: 0.5}},
 		}),
 			[]byte{0x85, 0xA7, 0x62, 0x6F, 0x6F, 0x6C, 0x65, 0x61, 0x6E, 0xC3, 0xA4, 0x6E, 0x75, 0x6C, 0x6C, 0xC0, 0xA6, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0xA7, 0x66, 0x6F, 0x6F, 0x20, 0x62, 0x61, 0x72, 0xA3, 0x69, 0x6E, 0x74, 0xFF, 0xA5, 0x66, 0x6C, 0x6F, 0x61, 0x74, 0xCB, 0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 		},
 	}
-	testMapTypeWriteTo(t, data)
+	testTypeWriteTo(t, data)
 }
 
 func TestMap_WriteTo_Nested(t *testing.T) {
 	data := []writeTestData{
-		{Map(map[MessagePackType]MessagePackType{
-			String("boolean"): Boolean(true),
-			String("null"):    Nil{},
-			String("string"):  String("foo bar"),
-			String("array"): Array([]MessagePackType{
+		{Map([]MessagePackMap{
+			{String("boolean"), Boolean(true)},
+			{String("null"), Nil{}},
+			{String("string"), String("foo bar")},
+			{String("array"), Array([]MessagePackType{
 				String("foo"),
 				String("bar"),
-			}),
-			String("int"):   Int(-2),
-			String("float"): Float{F: 0.5},
+			})},
+			{String("int"), Int(-2)},
+			{String("float"), Float{F: 0.5}},
 		}),
 			[]byte{0x86, 0xA7, 0x62, 0x6F, 0x6F, 0x6C, 0x65, 0x61, 0x6E, 0xC3, 0xA4, 0x6E, 0x75, 0x6C, 0x6C, 0xC0, 0xA6, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0xA7, 0x66, 0x6F, 0x6F, 0x20, 0x62, 0x61, 0x72, 0xA5, 0x61, 0x72, 0x72, 0x61, 0x79, 0x92, 0xA3, 0x66, 0x6F, 0x6F, 0xA3, 0x62, 0x61, 0x72, 0xA3, 0x69, 0x6E, 0x74, 0xFE, 0xA5, 0x66, 0x6C, 0x6F, 0x61, 0x74, 0xCB, 0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 		},
-		{Map(map[MessagePackType]MessagePackType{
-			String("int"):     Int(-2),
-			String("float"):   Float{F: 0.5},
-			String("boolean"): Boolean(true),
-			String("null"):    Nil{},
-			String("string"):  String("foo bar"),
-			String("array"): Array([]MessagePackType{
+		{Map([]MessagePackMap{
+			{String("int"), Int(-2)},
+			{String("float"), Float{F: 0.5}},
+			{String("boolean"), Boolean(true)},
+			{String("null"), Nil{}},
+			{String("string"), String("foo bar")},
+			{String("array"), Array([]MessagePackType{
 				String("foo"),
 				String("bar"),
-			}),
-			String("object"): Map(map[MessagePackType]MessagePackType{
-				String("foo"): Int(-1),
-				String("bar"): Float{F: 0.5},
-			}),
+			})},
+			{String("object"), Map([]MessagePackMap{
+				{String("foo"), Int(-1)},
+				{String("bar"), Float{F: 0.5}},
+			})},
 		}),
 			[]byte{0x87, 0xA3, 0x69, 0x6E, 0x74, 0xFE, 0xA5, 0x66, 0x6C, 0x6F, 0x61, 0x74, 0xCB, 0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA7, 0x62, 0x6F, 0x6F, 0x6C, 0x65, 0x61, 0x6E, 0xC3, 0xA4, 0x6E, 0x75, 0x6C, 0x6C, 0xC0, 0xA6, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0xA7, 0x66, 0x6F, 0x6F, 0x20, 0x62, 0x61, 0x72, 0xA5, 0x61, 0x72, 0x72, 0x61, 0x79, 0x92, 0xA3, 0x66, 0x6F, 0x6F, 0xA3, 0x62, 0x61, 0x72, 0xA6, 0x6F, 0x62, 0x6A, 0x65, 0x63, 0x74, 0x82, 0xA3, 0x66, 0x6F, 0x6F, 0xFF, 0xA3, 0x62, 0x61, 0x72, 0xCB, 0x3F, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 		},
 	}
-	testMapTypeWriteTo(t, data)
+	testTypeWriteTo(t, data)
 }
 
 func TestBinary_WriteTo_Map16(t *testing.T) {
@@ -80,19 +80,21 @@ func TestBinary_WriteTo_Map16(t *testing.T) {
 	expected[2] = 0xE8 // 1000 (Big Endian)
 	e := bytes.NewBuffer(expected)
 
-	input := make(map[MessagePackType]MessagePackType)
-	for i := 0; i < 1000; i++ {
-		k := Int(i)
-		v := Boolean(rand.Uint32()%2 == 0)
-		input[k] = v
-		_, _ = k.WriteTo(e)
-		_, _ = v.WriteTo(e)
+	input := make([]MessagePackMap, 1000)
+	for i := range input {
+		elem := MessagePackMap{
+			key:   Int(i),
+			value: Boolean(rand.Uint32()%2 == 0),
+		}
+		input[i] = elem
+		_, _ = elem.key.WriteTo(e)
+		_, _ = elem.value.WriteTo(e)
 	}
 
 	data := []writeTestData{
 		{Map(input), e.Bytes()},
 	}
-	testMapTypeWriteTo(t, data)
+	testTypeWriteTo(t, data)
 }
 
 func TestBinary_WriteTo_Map32(t *testing.T) {
@@ -104,17 +106,19 @@ func TestBinary_WriteTo_Map32(t *testing.T) {
 	expected[4] = 0x80
 	e := bytes.NewBuffer(expected)
 
-	input := make(map[MessagePackType]MessagePackType)
-	for i := 0; i < 80000; i++ {
-		k := Int(i)
-		v := Boolean(rand.Uint32()%2 == 0)
-		input[k] = v
-		_, _ = k.WriteTo(e)
-		_, _ = v.WriteTo(e)
+	input := make([]MessagePackMap, 80000)
+	for i := range input {
+		elem := MessagePackMap{
+			key:   Int(i),
+			value: Boolean(rand.Uint32()%2 == 0),
+		}
+		input[i] = elem
+		_, _ = elem.key.WriteTo(e)
+		_, _ = elem.value.WriteTo(e)
 	}
 
 	data := []writeTestData{
 		{Map(input), e.Bytes()},
 	}
-	testMapTypeWriteTo(t, data)
+	testTypeWriteTo(t, data)
 }
--- internal/types/test.go
+++ internal/types/test.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"bytes"
-	"sort"
 	"testing"
 )
 
@@ -21,32 +20,6 @@ func testTypeWriteTo(t *testing.T, data []writeTestData) {
 		}
 
 		result := buffer.Bytes()
-
-		if !bytes.Equal(result, test.expected) {
-			t.Errorf("Invalid result. Function returned %v. Expected %v.", result, test.expected)
-		}
-	}
-}
-
-func testMapTypeWriteTo(t *testing.T, data []writeTestData) {
-	for _, test := range data {
-		var buffer bytes.Buffer
-
-		_, err := test.input.WriteTo(&buffer)
-		if err != nil {
-			t.Errorf(err.Error())
-		}
-
-		result := buffer.Bytes()
-
-		// Map encoding is not ordered, so we can't compare data byte per byte
-		if len(result) != len(test.expected) || result[0] != test.expected[0] {
-			t.Errorf("Length different than expected")
-		}
-
-		// Check bytes by removing correct data
-		sort.Slice(result, func(i int, j int) bool { return result[i] < result[j] })
-		sort.Slice(test.expected, func(i int, j int) bool { return test.expected[i] < test.expected[j] })
 
 		if !bytes.Equal(result, test.expected) {
 			t.Errorf("Invalid result. Function returned %v. Expected %v.", result, test.expected)
