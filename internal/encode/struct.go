1e6e0aad753c208decbebe7149141f582d29e75e
ErikPelli <erikpellizzon1@gmail.com>
2022-02-25 21:55:54 +0100

    Add a temporary implementation of struct Len() [to be rewritten]

--- internal/encode/struct.go
+++ internal/encode/struct.go
@@ -13,8 +13,56 @@ type EncodingStruct struct {
 }
 
 func (e EncodingStruct) Len() int {
-	// TODO
-	return 0
+	// TODO Improve Struct Len() implementation
+	vStruct := reflect.Value(e.Struct)
+	// Remove pointers to struct
+	for !vStruct.IsZero() && vStruct.Kind() == reflect.Ptr {
+		vStruct = vStruct.Elem()
+	}
+
+	result := make(types.Map, vStruct.NumField())
+
+	for i := 0; i < vStruct.NumField(); i++ {
+		field := vStruct.Type().Field(i)
+		tag := field.Tag.Get("sbor")
+
+		if !field.IsExported() {
+			continue
+		}
+
+		name := field.Name
+		omitempty := false
+
+		if tag != "" {
+			options := strings.Split(tag, ",")
+
+			if len(options) == 1 {
+				if options[0] == "-" {
+					continue
+				} else {
+					name = options[0]
+				}
+			}
+
+			if len(options) >= 2 {
+				if options[1] == "omitempty" {
+					omitempty = true
+				} else if options[1] == "" {
+					name = options[0]
+				}
+			}
+		}
+
+		fieldValue := vStruct.Field(i)
+		if omitempty && fieldValue.IsZero() {
+			continue
+		}
+
+		result[i].Key = types.String(name)
+		result[i].Value = TypeWrapper(fieldValue)
+	}
+
+	return result.Len()
 }
 
 func (e EncodingStruct) WriteTo(w io.Writer) (int64, error) {
