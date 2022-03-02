7297764204603086bfcb7badbcb9c6ec2afdf054
ErikPelli <erikpellizzon1@gmail.com>
2022-03-02 14:51:00 +0100

    Add tag parse struct

---
+++ internal/tag/tag_test.go
@@ -0,0 +1,39 @@
+package tag
+
+import (
+	"testing"
+)
+
+func TestSkipTag(t *testing.T) {
+	name, options := ParseTag("-")
+	if name != "-" || string(options) != "" {
+		t.Errorf("Invalid value. Name: %s, Options: %s", name, options)
+	}
+
+	if options.Contains("") {
+		t.Error("Invalid tag contains, Empty string must be false.")
+	}
+}
+
+func TestHyphenTag(t *testing.T) {
+	name, options := ParseTag("-,b,c,d")
+	if name != "-" || string(options) != "b,c,d" {
+		t.Errorf("Invalid value. Name: %s, Options: %s", name, options)
+	}
+
+	if !(options.Contains("b") && options.Contains("c") && options.Contains("d")) {
+		t.Error("Invalid tag contains")
+	}
+
+}
+
+func TestTrailingComma(t *testing.T) {
+	name, options := ParseTag("trailing,")
+	if name != "trailing" || string(options) != "" {
+		t.Errorf("Invalid value. Name: %s, Options: %s", name, options)
+	}
+
+	if options.Contains("") {
+		t.Error("Invalid tag contains, Empty string must be false.")
+	}
+}
