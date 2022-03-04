3d94ad33b857a21e2ff2b215035ad689cf640555
ErikPelli <erikpellizzon1@gmail.com>
2022-03-04 21:18:54 +0100

    Add messagepack types documentation

--- internal/types/types.go
+++ internal/types/types.go
@@ -75,20 +75,30 @@ type (
 	Struct  reflect.Value
 )
 
+// External MessagePack type
+type External struct {
+	Type byte
+	Data []byte
+}
+
+// MessagePackTypeEncoder contains the methods used to convert the type into bytes
 type MessagePackTypeEncoder interface {
 	Len() int
 	io.WriterTo
 }
 
+// MessagePackTypeDecoder contains the methods used to convert the bytes into the type
 type MessagePackTypeDecoder interface {
 	// TODO io.ReaderFrom
 }
 
+// MessagePackType is a MessagePack-compatible type
 type MessagePackType interface {
 	MessagePackTypeEncoder
 	MessagePackTypeDecoder
 }
 
+// MessagePackMap is a single Key-Value association
 type MessagePackMap struct {
 	Key   MessagePackType
 	Value MessagePackType
