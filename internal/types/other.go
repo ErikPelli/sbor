7e102945f476d64237491a16ea5355b9cc886592
ErikPelli <erikpellizzon1@gmail.com>
2022-03-26 23:28:48 +0100

    Add nil and boolean Write method

--- internal/types/other.go
+++ internal/types/other.go
@@ -1,6 +1,9 @@
 package types
 
-import "io"
+import (
+	"github.com/ErikPelli/sbor/internal/utils"
+	"io"
+)
 
 // Len returns the length of the MessagePack encoded null.
 func (n Nil) Len() int {
@@ -13,6 +16,11 @@ func (n Nil) Len() int {
 func (n Nil) WriteTo(w io.Writer) (int64, error) {
 	writtenBytes, err := w.Write([]byte{NilCode})
 	return int64(writtenBytes), err
+}
+
+func (n Nil) Write(p []byte) (int, error) {
+	// Do nothing, nil is identified in MessagePack type code
+	return 0, nil
 }
 
 // Len returns the length of the MessagePack encoded boolean.
@@ -34,4 +42,17 @@ func (b Boolean) WriteTo(w io.Writer) (int64, error) {
 
 	writtenBytes, err := w.Write([]byte{value})
 	return int64(writtenBytes), err
+}
+
+func (b *Boolean) Write(p []byte) (int, error) {
+	var err error
+	length := len(p)
+
+	if length == 1 {
+		*b = p[0] == True
+	} else {
+		err = utils.InvalidArgumentError{Desc: "invalid boolean length"}
+	}
+
+	return length, err
 }
